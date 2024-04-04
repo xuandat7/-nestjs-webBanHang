f6ff2b8ba71aafbd5104cbdc2c34f2203dc3d6f8
xuandat7 <xuandattran2003@example.com>
2024-04-04 16:45:17 +0700

    generate module fe-be

---
+++ src/backend/backend.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ArticleModule } from './article/article.module';
+import { CategoryModule } from './category/category.module';
+import { MenuModule } from './menu/menu.module';
+import { ProductModule } from './product/product.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [ArticleModule, CategoryModule, MenuModule, ProductModule, TransactionModule, UserModule]
+})
+export class BackendModule {}
